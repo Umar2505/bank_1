ff0afad31868b641f06c1789b7612dbb67acf988
Umar <rustamovumar0@gmail.com>
2021-11-20 22:40:35 +0500

    types v1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Umar2505/bank
+
+go 1.17
---
+++ pkg/types/types.go
@@ -0,0 +1,41 @@
+package types
+
+type Money int64
+
+type Currency string
+
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+type PAN string
+
+type Card struct {
+	ID         int
+	PAN        PAN
+	Balance    Money
+	MinBalance Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+}
+
+type Category string
+
+type Status string
+
+const (
+	StatusOk Status = "OK"
+	StatusFail Status = "FAIL"
+	StatusInProgress Status = "INPROGRESS"
+)
+
+type Payment struct {
+	ID       int
+	Amount   Money
+	Category Category
+	Status Status
+}
